4a14bdfffa216a22a17e012884c25c4cdb9a6ee0
Daniel Duecker <daniel.duecker@tum.de>
2024-07-20 14:46:41 -0400

    update ahoi_interface with piggybacked anchor positions

--- scripts/ahoi_interface.py
+++ scripts/ahoi_interface.py
@@ -13,12 +13,15 @@ import time
 from ahoi.modem.modem import Modem
 
 class AhoiInterface():
-    def __init__(self, base_id, my_id):
+    def __init__(self, my_id, base_id, dev="/dev/ttyUSB0"):
         self.myModem = Modem()
-        self.myModem.connect("/dev/ttyUSB0")
+        self.myModem.connect(dev)
 
-        self.base_id = base_id  # id of the (mobile) base modem
         self.my_id = my_id      # id of this modem 
+        self.base_id = base_id  # id of the (mobile) base modem
+
+        self.dsn = 0 # init packet counter
+        
 
         SPEED_OF_SOUND = 1500 
         self.speed_of_sound = SPEED_OF_SOUND # in m/s
@@ -36,29 +39,38 @@ class AhoiInterface():
         self.myModem.receive(thread=True)
 
     def trigger_range_poll(self):
+        self.dsn += 1 # increase packet sequence
+        dst_msg = 255 # 255 = broadcast
+        
         self.myModem.send(src=self.base_id,
-                            dst=255,#self.anchor_ids[self.i],
+                            dst=dst_msg,#self.anchor_ids[self.i],
                             status=2,               # status 2 trigger HW-range-ACK
                             type=0x00,              # type for ranging poll - that is auto-replied by the receipent
-                            payload=bytearray())
-        print("sent range poll ...")
+                            payload=bytearray(),
+                            dsn=self.self.dsn)
+        
+        print(f"[Base with ID {self.my_id}] sent range poll to ID {dst_msg} sqn {self.dsn} ...")
     
     def trigger_pos_range_poll(self, dst_modem_id):
+        self.dsn += 1 # increase packet sequence
+
         self.myModem.send(src=self.base_id,
                             dst=dst_modem_id,       # id of destination modem
                             status=2,
                             type=0x7A,              # type for ranging+pos poll [own]
-                            payload=bytearray())
-        print("sent range-pos poll ...")
+                            payload=bytearray(),
+                            dsn=self.dsn)
+        print(f"[Base with ID {self.my_id}] sent range poll to ID {dst_modem_id} sqn {self.dsn} ...")
     
 
 
     def rangingPosCallbackPoll(self, pkt):
-        if pkt.header.type == 0x7A and pkt.header.len > 0 and pkt.header.dst == self.my_id: # if poll for range+pos is received
-            src = pkt.header.src
+        if pkt.header.type == 0x7A and pkt.header.dst == self.my_id: # if poll for range+pos is received
+            src = pkt.header.src # read source id
+            dsn_poll = pkt.header.dsn # read packet sequence number from poll
 
-            #time.sleep(0.5)  # wait before sending position, ranging ACK is sent before
-            my_position_x = 42
+            time.sleep(1)  # wait before sending position, ranging ACK is sent before
+            my_position_x = 42 + int(np.random.rand()*10)
             my_position_y = 84
             position = my_position_x.to_bytes(2, 'big', signed=True) + my_position_y.to_bytes(2, 'big', signed=True)
 
@@ -68,21 +80,18 @@ class AhoiInterface():
                               status=0, 
                               payload=position)     # transmit anchor position (type 0x7D)
             
-            print(f"\n[ANCHOR] Sent position: {my_position_x}, {my_position_y}\n")
+            print(f"[Anchor ID {self.my_id}] Received poll seq_num {dsn_poll} from Anchot ID {src} - reply my position: {my_position_x}, {my_position_y}")
 
 
     def rangingPosCallbackAck(self, pkt):
         if pkt.header.type == 0x7D and pkt.header.len > 0 and pkt.header.dst == self.my_id: # if poll for range+pos is received
-            src = pkt.header.src
+            src = pkt.header.src # read source id
+            dsn_poll = pkt.header.dsn # read packet sequence number from poll
 
-            #position = my_position_x.to_bytes(2, 'big', signed=True) + my_position_y.to_bytes(2, 'big', signed=True)
+            position_x = int.from_bytes(pkt.payload[0:2], 'big', signed=True) * 1e-2
+            position_y = int.from_bytes(pkt.payload[2:4], 'big', signed=True) * 1e-2
             
-            position_y = int.from_bytes(pkt.payload[0:2], 'big', signed=True) * 1e-2
-            position_x = int.from_bytes(pkt.payload[2:4], 'big', signed=True) * 1e-2
-
-            
-            
-            print(f"\n[ANCHOR] Received position: {position_x}, {position_y}\n")
+            print(f"[Anchor ID {self.my_id}] Received reply to my poll {dsn_poll} from ANCHOR ID{src}: Received position: {position_x}, {position_y}")
 
 
 
@@ -93,86 +102,23 @@ class AhoiInterface():
         # check if we have received a ranging ack
         if pkt.header.type == 0x7F and pkt.header.len > 0:
             src = pkt.header.src
-            # if src == self.anchor_ids[
-            #         0]:  # map anchor ids auf 0,1,2 Logik von estimator
-            #     id = 0
-            # elif src == self.anchor_ids[1]:
-            #     id = 1
-            # elif src == self.anchor_ids[2]:
-            #     id = 2
-            # dst = pkt.header.dst
-            # type = pkt.header.type
-            # status = pkt.header.status
-            #self.publish_received_packets(id, dst, type, status)
+            dsn = pkt.header.dsn
 
             tof = 0
             for i in range(0, 4):
                 tof = tof * 256 + pkt.payload[i]
             distance = tof * 1e-6 * self.speed_of_sound
-            # self.get_logger().info(f"distance to {src}: %6.2f" % (distance))
 
-
-            print("measured distance to "+ str(src) + ": " + str(distance))
-            #self.publish_distance(id, distance)
-
-        # check if we have received position update
-        if pkt.header.type == 0x7D and pkt.header.len > 0:
-            src = pkt.header.src
-            # if src == self.anchor_ids[0]:
-            #     id = 0
-            # elif src == self.anchor_ids[1]:
-            #     id = 1
-            # elif src == self.anchor_ids[2]:
-            #     id = 2
-            dst = pkt.header.dst
-            type = pkt.header.type
-            status = pkt.header.status
-            self.publish_received_packets(id, dst, type, status)
-
-            position_y = int.from_bytes(
-                pkt.payload[0:2],
-                'big',  # position north
-                signed=True) * 1e-2
-            position_x = int.from_bytes(
-                pkt.payload[2:4],
-                'big',  # position east
-                signed=True) * 1e-2
-            # self.get_logger().info(
-            #     f"position of anchor {src}: x = {position_x}, y = {position_y}")
-
-            self.publish_anchor_pose(id, position_x, position_y)
-
-        # # check if we have received initial position
-        # if pkt.header.type == 0x7B and pkt.header.len > 0:
-        #     src = pkt.header.src
-        #     if src == self.anchor_ids[0]:
-        #         id = 0
-        #     elif src == self.anchor_ids[1]:
-        #         id = 1
-        #     elif src == self.anchor_ids[2]:
-        #         id = 2
-        #     dst = pkt.header.dst
-        #     type = pkt.header.type
-        #     status = pkt.header.status
-        #     self.publish_received_packets(id, dst, type, status)
-
-        #     position_y = int.from_bytes(pkt.payload[0:2], 'big',
-        #                                 signed=True) * 1e-2
-        #     position_x = int.from_bytes(pkt.payload[2:4], 'big',
-        #                                 signed=True) * 1e-2
-        #     # self.get_logger().info(
-        #     #     f"initial position of anchor {src}: x = {position_x}, y = {position_y}"
-        #     # )
-        #     self.initial_position_bool[id] = True
-
-        #     self.publish_anchor_pose(id, position_x, position_y)
+            print(f"[Anchor ID {self.my_id}] Received auto-TOF from Anchor ID {src} | TODO/Check DSN {dsn}| - measured distance {distance}")
 
 def main():
     
     try:
-        my_base  = AhoiInterface(base_id=1,my_id=0)
+        my_base  = AhoiInterface(base_id=87,my_id=0,dev="/dev/ttyUSB1")
         while(True):
-            my_base.trigger_range_poll()
+            #my_base.trigger_range_poll()
+            
+            #print("still alive...")my_base.trigger_pos_range_poll(dst_modem_id=87)
             time.sleep(1)
 
     except KeyboardInterrupt:
